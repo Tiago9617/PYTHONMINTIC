1793aafd9c33d0c5469c01dc54c5b958d24c0fcf
Tiago9617 <santiagorojas_1996@hotmail.com>
2022-05-05 11:52:42 -0500

    Se agregaron cambios como un menu y excepciones para el manejo de errores

--- CalcularCDT/CalcularCDT.py
+++ CalcularCDT/CalcularCDT.py
@@ -25,3 +25,37 @@ def CDT(usuario: str, capital: int, tiempo: int):
 print(CDT("AB1012", 1000000, 3)) 
 print(CDT("ER3366", 650000, 2)) 
 
+if __name__ == "__main__":
+
+  while(True):
+
+        try:
+            opcion = int(input("""Hola este es un aplicativo que te permite calcular la cantidad de intereses ganados 
+            Informacion adicional:
+            Si el usuario decide retirar su dinero en menos de dos meses se le aplica una penalidad del 2%
+
+            1) Calcular usuarios con ganancia mayores a 3 meses
+            2) calcular usuarios con ganancias menores a 2 meses
+            3) listar usuarios con ganancia mayores a 3 meses
+            4) listar usuarios con ganancias menores a 2 meses
+            5) listar todos los usuarios
+            6) Salir del programa
+            
+            Digite una opcion numerica: """))
+            if opcion == 1:
+                print(CDT("AB1012", 1000000, 3))
+            elif opcion == 2:
+                print(CDT("ER3366", 650000, 2)) 
+            elif opcion == 3:
+                pass
+            elif opcion == 4:
+                pass
+            elif opcion == 5:
+                pass
+            elif opcion == 6:
+                break
+            else:
+                print("digite una opcion valida del menu \n\n")
+        except ValueError:
+            print("Por favor digite una opcion numerica \n\n")
+
