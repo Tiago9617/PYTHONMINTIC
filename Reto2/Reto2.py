c9571638a83f66b0c5152593fe24d0c0da85231a
Tiago9617 <santiagorojas_1996@hotmail.com>
2022-05-09 21:12:06 -0500

    Reto terminado, es necesario separar la palabra Carroschocones

--- Reto2/Reto2.py
+++ Reto2/Reto2.py
@@ -15,14 +15,14 @@ los clientes podrán ingresar dependiendo de su edad, posteriormente el parque d
 decidido otorgar un descuento al valor de la boleta si cumple con el rango de edad y es la primera
 vez que el cliente ingresa.
 
-Atracción   Valor de boleta     Edad            Primer  Ingreso Descuento
+Atracción   Valor de boleta     Edad            Primer  Ingreso     Descuento
                                 (años) 
-X-Treme     20000               >18 años        True    5% del valor de la boleta
+X-Treme     20000               >18 años        True                5% del valor de la boleta
 Carros
-chocones    5000                >=15 y          True    7% del valor de la boleta
+chocones    5000                >=15 y          True                7% del valor de la boleta
                                 <=18 
 Sillas
-voladoras   10000               >=7 y < 15      True    5% del valor de la boleta
+voladoras   10000               >=7 y < 15      True                5% del valor de la boleta
 
 Esta función debe retornar un nuevo diccionario con las llaves nombre, edad, atracción,
 primer_ingreso, total_boleta y apto del cliente:
@@ -39,47 +39,144 @@ lo contrario se pagará el valor neto de la boleta.
 """
 
 informacion = {
+
 "id_cliente" : 1,
 "nombre" : "Johana Fernandez",
-"edad" : 14,
+"edad" : 20,
 "primer_ingreso" : True
+
+}
+informacion1 = {
+
+"id_cliente" : 1,
+"nombre" : "Johana Fernandez",
+"edad" : 20,
+"primer_ingreso" : False
+
+}
+
+informacion2 = {
+    
+"id_cliente" : 2,
+"nombre" : "Gloria Suarez",
+"edad" : 6,
+"primer_ingreso" : True
+
+}
+
+informacion3 = {
+
+"id_cliente" : 3,
+"nombre" : "Tatiana Suarez",
+"edad" : 17,
+"primer_ingreso" : True
+
+}
+
+informacion4 = {
+
+"id_cliente" : 3,
+"nombre" : "Tatiana Suarez",
+"edad" : 17,
+"primer_ingreso" : False
+
+}
+
+informacion5 = {
+    
+"id_cliente" : 4,
+"nombre" : "Tatiana Suarez",
+"edad" : 8,
+"primer_ingreso" : True
+
+}
+
+informacion6 = {
+
+"id_cliente" : 4,
+"nombre" : "Tatiana Suarez",
+"edad" : 8,
+"primer_ingreso" : False
                     
 }
 
-print(informacion["edad"])
-def cliente(informacion:dict)-> dict:
-    costo_total = 0
+
+def cliente(informacion:dict):
     informacion_ingreso = {}
+    
     if( informacion["edad"] > 18 and informacion["primer_ingreso"] == True):
-        costo_total = 20000 - (20000*0.05)
+
         informacion_ingreso["nombre"] = informacion["nombre"]
         informacion_ingreso["edad"] = informacion["edad"]
-        informacion_ingreso["atraccion"] = "Sillas voladoras"
-        informacion_ingreso["primer_ingreso"] = True
-        print(costo_total)
+        informacion_ingreso["atraccion"] = "X-Treme"
+        informacion_ingreso["apto"] = True
+        informacion_ingreso["primer_ingreso"] = informacion["primer_ingreso"]
+        informacion_ingreso["total_boleta"] = 20000 - (20000*0.05)
+        
+        
     elif(informacion["edad"] > 18 and informacion["primer_ingreso"] == False):
-        costo_total = 20000 
+ 
         informacion_ingreso["nombre"] = informacion["nombre"]
         informacion_ingreso["edad"] = informacion["edad"]
-        informacion_ingreso["atraccion"] = "Sillas voladoras"
-        informacion_ingreso["primer_ingreso"] = False
-        print(costo_total)
-    elif( informacion["edad"] >= 15 and informacion["edad"] <= 18):
-        costo_total = 5000 - (5000*0.07)
-        print(costo_total)
+        informacion_ingreso["atraccion"] = "X-Treme"
+        informacion_ingreso["apto"] = True
+        informacion_ingreso["primer_ingreso"] = informacion["primer_ingreso"]
+        informacion_ingreso["total_boleta"] = 20000
+        
+        
+    elif( informacion["edad"] >= 15 and informacion["edad"] <= 18 and informacion["primer_ingreso"] == True) :
+
         informacion_ingreso["nombre"] = informacion["nombre"]
         informacion_ingreso["edad"] = informacion["edad"]
         informacion_ingreso["atraccion"] = "Carros chocones"
-        informacion_ingreso["primer_ingreso"] = "Sillas voladoras"
-    elif (informacion["edad"] >= 7 and informacion["edad"] < 15):
-        costo_total = 10000 - (10000*0.05)
-        print(costo_total)
+        informacion_ingreso["apto"] = True
+        informacion_ingreso["primer_ingreso"] = informacion["primer_ingreso"]
+        informacion_ingreso["total_boleta"] = 5000 - (5000*0.07)
+        
+    elif( informacion["edad"] >= 15 and informacion["edad"] <= 18 and informacion["primer_ingreso"] == False) :
+
+        informacion_ingreso["nombre"] = informacion["nombre"]
+        informacion_ingreso["edad"] = informacion["edad"]
+        informacion_ingreso["atraccion"] = "Carros chocones"
+        informacion_ingreso["apto"] = True
+        informacion_ingreso["primer_ingreso"] = informacion["primer_ingreso"]
+        informacion_ingreso["total_boleta"] = 5000
+    
+    elif (informacion["edad"] >= 7 and informacion["edad"] < 15 and informacion["primer_ingreso"] == True):
+
         informacion_ingreso["nombre"] = informacion["nombre"]
         informacion_ingreso["edad"] = informacion["edad"]
         informacion_ingreso["atraccion"] = "Sillas voladoras"
-        informacion_ingreso["primer_ingreso"] = "Sillas voladoras"
+        informacion_ingreso["apto"] = True
+        informacion_ingreso["primer_ingreso"] = informacion["primer_ingreso"]
+        informacion_ingreso["total_boleta"] = 10000 - (10000*0.05)
+    
+    elif (informacion["edad"] >= 7 and informacion["edad"] < 15 and informacion["primer_ingreso"] == False):
+
+        informacion_ingreso["nombre"] = informacion["nombre"]
+        informacion_ingreso["edad"] = informacion["edad"]
+        informacion_ingreso["atraccion"] = "Sillas voladoras"
+        informacion_ingreso["apto"] = True
+        informacion_ingreso["primer_ingreso"] = informacion["primer_ingreso"]
+        informacion_ingreso["total_boleta"] = 10000
+        
+    elif (informacion["edad"] < 7):
+        informacion_ingreso["nombre"] = informacion["nombre"]
+        informacion_ingreso["edad"] = informacion["edad"]
+        informacion_ingreso["atraccion"] = "N/A"
+        informacion_ingreso["apto"] = False
+        informacion_ingreso["primer_ingreso"] = informacion["primer_ingreso"]
+        informacion_ingreso["total_boleta"] = "N/A"
+    
+    
     return informacion_ingreso
     
 if __name__ == '__main__':
     
-    print(cliente(informacion))
+    print(str(cliente(informacion)) + "\n\n")
+    print(str(cliente(informacion1)) + "\n\n")
+    print(str(cliente(informacion2)) + "\n\n")
+    print(str(cliente(informacion3)) + "\n\n")
+    print(str(cliente(informacion4)) + "\n\n")
+    print(str(cliente(informacion5)) + "\n\n")
+    print(str(cliente(informacion6)) + "\n\n")
