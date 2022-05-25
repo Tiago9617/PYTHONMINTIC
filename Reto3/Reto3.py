3506cc359365983a105721aabb3da16f6c7a4e41
Tiago9617 <santiagorojas_1996@hotmail.com>
2022-05-24 20:50:56 -0500

    challenge 3

--- Reto3/Reto3.py
+++ Reto3/Reto3.py
@@ -1,10 +1,5 @@
 """
 
-def AutoPartes(ventas:list):
-    dic = {} #crear el diccionario vacío
-    for i in range(len(ventas)):
-        dic[i] = #asignar ventas al diccionario
-    return #diccionario
    
 def consultaRegistro(ventas,idProducto):
     newDic = {}#crear un nuevo diccionario
@@ -70,34 +65,42 @@ ventas = ([
  (2010,'bujía', 'ER6523',9,36,'Pedro Montes',1243,'12/06/2020'),
  (3578,'tijera', 'QW8523',1,128,'Pedro Faria',1456,'12/06/2020'),
  (9251,'piñón', 'EN5698',2,8,'Juan Peña',565,'12/06/2020')])
-def AutoPartes(ventas: list):
-    dict = {
-        "idProducto": (ventas[0][0], ventas[1][0], ventas[2][0], ventas[3][0], ventas[4][0]),
-        "dProducto":  (ventas[0][1], ventas[1][1], ventas[2][1], ventas[3][1], ventas[4][1]),
-        "pnProducto": (ventas[0][2], ventas[1][2], ventas[2][2], ventas[3][2], ventas[4][2]),
-        "cvProducto": (ventas[0][3], ventas[1][3], ventas[2][3], ventas[3][3], ventas[4][3]),
-        "sproducto":  (ventas[0][4], ventas[1][4], ventas[2][4], ventas[3][4], ventas[4][4]),
-        "nComprador": (ventas[0][5], ventas[1][5], ventas[2][5], ventas[3][5], ventas[4][5]),
-        "cComprador": (ventas[0][6], ventas[1][6], ventas[2][6], ventas[3][6], ventas[4][6]),
-        "fventa":     (ventas[0][7], ventas[1][7], ventas[2][7], ventas[3][7], ventas[4][7]),
-    
-    }
-    return dict
-    
-def consultaRegistro(ventas: dict, idProducto):
-    mensaje = ""
-    bandera = False
-    for clave, valor in ventas.items():
-        for numero in range(0, 5):
+ventas1=([
+ (5489,'tornillo', 'RS8512',2,33,'Julio Perez',3654213,'13/06/2020'),
+ (3215,'zocalo', 'UM8587',2,125,'Laura Macias',1256321,'13/06/2020'),
+ (3698,'biela', 'PT3218',1,78,'Luis Peña',14565487,'13/06/2020'),
+ (8795,'cilindro', 'AZ8794',2,96,'Carlos Casio',5612405,'13/06/2020')])
+ventas2=([
+ (9852,'Culata', 'XC9875',2,165,'Luis Molero',3455846,'14/06/2020'),
+ (9852,'Culata', 'XC9875',2,165,'Jose Mejia',1355846,'14/06/2020'),
+ (2564,'Cárter', 'PT29872',2,32,'Peter Cerezo',8545436,'14/06/2020'),
+ (5412,'válvula', 'AZ8798',2,11,'Juan Peña',568975,'14/06/2020')])
 
-            if valor[0][numero] == idProducto: 
-                mensaje = mensaje + f"Producto consultado : {idProducto} Descripción bujía #Parte MS9512 Cantidad vendida 4 Stock 15 Comprador Carlos Rondon Documento 1256 Fecha Venta 12/06/2020"
-                break
-        if(bandera == True):
-            bandera = False
-            break
-            
-         
+def AutoPartes(ventas:list):
+    dict = {} 
+    for i in range(len(ventas)):
+        dict[i] = ventas[i]
+    return dict
+def consultaRegistro(ventas,idProducto):
+    nuevoDict = {}
+    respuesta= ""
+    for i in ventas:
+        if ventas[i][0][0] == idProducto:
+            nuevoDict[i] = ventas[i]      
+    if len(nuevoDict) == 0:
+        respuesta = "No hay registro de venta de ese producto"
+    else:
+        for i in nuevoDict:
+            respuesta += "Producto consultado : {} Descripción {} #Parte {} Cantidad vendida {} Stock {} Comprador {} Documento {} Fecha Venta {}\n".format(ventas[i][0][0],ventas[i][0][1],ventas[i][0][2],ventas[i][0][3],ventas[i][0][4],ventas[i][0][5],ventas[i][0][6],ventas[i][0][7])      
+    return print(respuesta)
+               
 if __name__ == "__main__":
-    while(True):
-        consultaRegistro(AutoPartes(ventas), 2010)
+    ##print(AutoPartes1(ventas))
+    
+    """print(AutoPartes(ventas),"\n\n")
+    
+    print(AutoPartes(ventas1),"\n\n")
+    print(AutoPartes(ventas2),"\n\n")"""
+    consultaRegistro(AutoPartes(ventas),2010)
+    ##consultaRegistro(AutoPartes(ventas1),2001)
+    ##consultaRegistro(AutoPartes(ventas2),9852)
