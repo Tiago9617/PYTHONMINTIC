c93fb6104148714a62acde3c2de6f9c7c23ee841
Tiago9617 <santiagorojas_1996@hotmail.com>
2022-06-06 12:57:46 -0500

    terminacion del reto 4

--- Reto4/Reto4.py
+++ Reto4/Reto4.py
@@ -25,26 +25,36 @@ Recuerde, que la salida no debe tener más de dos decimales.
 """
 
 def ordenes(rutinaContable):
- from functools import reduce
- sumatoria = lambda x, y : x + y
- print(sumatoria)
- calculo_multi = lambda x, y : x * y
- print(calculo_multi)
- print('----------------- Inicio Registro diario --------------------------')
- for j in range(len(rutinaContable)):
-    s_adicion = []
-    valores = []
-    for i in range[rutinaContable[j]-1]:
-        s_adicion.append(rutinaContable[j][i+1][2])
-        valores.append(rutinaContable[j][i+1][1])
-    valores = map(calculo_multi, s_adicion, valores)
-    total = reduce(sumatoria, valores)
-    if total < 600000:
-        total = total + 25000
-    mensaje = f"La factura {rutinaContable[j][0]} tiene un total en pesos de {total:,.2f}"
+    from functools import reduce
+    print('------------------------ Inicio Registro diario ---------------------------------')
+    for i in range(len(rutinaContable)):
+        preciosUnitarios = []
+        cantidadProductos = []
+        for j in range(len(rutinaContable[i])-1):
+            preciosUnitarios.append(rutinaContable[i][j+1][2])
+            cantidadProductos.append(rutinaContable[i][j+1][1])
+        totalCostoProductos = map(lambda x, y : x * y, preciosUnitarios, cantidadProductos)
+        total = reduce(lambda x, y : x + y, totalCostoProductos)
+        if total < 600000:
+            total = total + 25000
+        mensaje = f"La factura {rutinaContable[i][0]} tiene un total en pesos de {total:,.2f}"
+        print(mensaje)
     
-    print(mensaje)
- 
- print('----------------- Fin Registro diario -----------------------------')
+    print('-------------------------- Fin Registro diario ----------------------------------')
+    
+lista = [
+    
+    [1201, ("5464", 4, 25842.99), ("7854",18,23254.99), ("8521", 9, 48951.95)],
+    [1202, ("8756", 3, 115362.58),("1112",18,2354.99)],
+    [1203, ("2547", 1, 125698.20), ("2635", 2, 135645.20), ("1254", 1, 13645.20),("9965", 5, 1645.20)],
+    [1204, ("9635", 7, 11.99), ("7733",11,18.99), ("88112", 5, 390.95)]
+    
+    ]
+lista1 = [
+    
+    [6587, ("3268", 4, 25842.99), ("8274",18,23254.99), ("6548", 9, 48951.95), ("2547", 5, 8951.95)]
+    
+]
 if __name__ == "__main__":
-    pass
+    ordenes(lista)
+    ordenes(lista1)
